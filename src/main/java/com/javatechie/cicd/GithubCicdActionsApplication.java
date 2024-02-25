8b7189f96100725d7808588c54000ea380cb768c
Mani <41326007+Manikanndan@users.noreply.github.com>
2024-02-25 20:52:32 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Hello World !!!";
+		return "Hello World, Testing Spring Project !!!";
 	}
 
 //	echo "# github-actions-example" >> README.md

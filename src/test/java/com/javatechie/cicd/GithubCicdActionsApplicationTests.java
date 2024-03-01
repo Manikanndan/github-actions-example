a93f3d98844d1376347b15e3f46e8ad3ae60ec53
Mrnatureboy <swavic@gmail.com>
2024-03-01 15:56:22 +0530

    Made changes

--- src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
+++ src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
@@ -6,8 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GithubCicdActionsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	void test1(){
+        System.out.println("Executing Addition Test");
+        assert 3 == MathsClass.add(1,2);
+    }
+
+    @Test
+    void test2(){
+        System.out.println("Executing Multi[lication] Test");
+        assert 12 == MathsClass.multiply(6,2);
+    }
 
 }

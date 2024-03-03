06f9f555e8a2a5eab8353b2f8811036def2675bc
Mani <41326007+Manikanndan@users.noreply.github.com>
2024-03-03 19:37:41 +0530

    Update GithubCicdActionsApplicationTests.java

--- src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
+++ src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GithubCicdActionsApplicationTests {
 
-	String message = "Hello World!...";
+	String message = "World!...";
 	
 	@Test 
 	void test1() {
